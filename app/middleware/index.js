ae77b332b2cfcf6cdc2284cb22fac990c9e88043
easa <eisanodehi@gmail.com>
2019-11-15 10:19:16 +0330

    refactor middleware/index
    the validation now gets the model of request

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -5,26 +5,15 @@ module.exports = function (opt) {
 	reqModel = opt.reqModel
 	resModel = opt.resModel
 	return app
-	// otherwise? (r, s, n) => { n() }
 }
 
 function app(req, res, next) {
 	let paramObject = {}
-	if (req.body)
-		Object.assign(paramObject, req.body)
-	if (req.params)
-		Object.assign(paramObject, req.param)
-
-	let err = validate(paramObject)
-
-	req.error = (!err || (typeof err === 'string' && err == ''))
-		? false
-		: err
-
-	// TODO: append send to model of resonse
-
+	if (req.body) Object.assign(paramObject, req.body)
+	if (req.params) Object.assign(paramObject, req.param)
 	req.model = Object.assign({}, reqModel)
 	res.model = Object.assign({}, resModel)
-
+	let err = validate(req.model, paramObject)
+	req.error = (!err || (typeof err === 'string' && err == '')) ? false : err
 	next()
 }
