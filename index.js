b6290d99ce987e10ed14c74f3609c31d938475e8
easa <eisanodehi@gmail.com>
2019-12-09 14:36:50 +0330

    bug fix - middleware - model undefined

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const validate = require('./validations/')
-let _viewModel = {}
+
 /**
  * validate req.params via the model you define
  * pass your model as parameter (viewModel)
  * @example
  * 		router = express()
  * 		router(juv(model))
- * @param {object} viewModel the model of expected params
- * also could contain the reqModel and resModel also could be the reqModel itself
+ * 
  * @example
- * {
+ * cosnt veiwModel = {
  * 	name : n => {
  * 		return (n.match(/^easa$/)) 
  * 			? true
@@ -17,23 +16,24 @@ let _viewModel = {}
  * 	},
  * 	pass : /^[0-9]{6,12}$/
  * } 
+ * @param {object} viewModel the model of expected params
+ * also could contain the reqModel and resModel also could be the reqModel itself
  * @returns {function} the middleware function to pass to express
  * @copyright easa
 */
 module.exports = function (viewModel) {
+	const model = (typeof viewModel === 'object') ? viewModel : {}
+	return (req, res, next) => {
+		req.model = model
+		
+		let paramObject = {}
+		if (req.body) Object.assign(paramObject, req.body)
+		if (req.params) Object.assign(paramObject, req.params)
 
-	if (typeof viewModel === 'object')
-		_viewModel =	viewModel
+		let err = validate(req.model, paramObject)
 
-	return app
-}
+		req.error = (err && typeof err === 'string' && err !== '') ? err : undefined
 
-function app(req, res, next) {
-	let paramObject = {}
-	if (req.body) Object.assign(paramObject, req.body)
-	if (req.params) Object.assign(paramObject, req.params)
-	req.model = Object.assign({}, _viewModel)
-	let err = validate(req.model, paramObject)
-	req.error = (!err || (typeof err === 'string' && err == '')) ? false : err
-	next()
+		next()
+	}
 }
