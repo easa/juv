bb36d8db5953bcb67754a6557d8eb1788f2631de
easa <eisanodehi@gmail.com>
2019-12-09 14:57:05 +0330

    refactor, smaller anatomy, it's more compact now

--- test/validations/validateReqModel.test.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-undef */
-const app = require('../../validations/validateReqModel')
-
-test('should be a function', () => {
-	expect(typeof app).toBe('function')
-})
--- test/validations/validateResModel.test.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-undef */
-const app = require('../../validations/validateResModel')
-
-test('should be a function', () => {
-	expect(typeof app).toBe('function')
-})
